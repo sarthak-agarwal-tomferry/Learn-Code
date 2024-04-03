c5abdcad81598b84b21aaf39a2e3c0ef758cd2c8
Sarthak Agarwal <sarthak.agarwal@intimetec.com>
2024-04-03 20:15:24 +0530

    Assignment5 - Updated try catch block

--- Assignment5/CarAccidentErrorHandling.java
+++ Assignment5/CarAccidentErrorHandling.java
@@ -1,41 +1,42 @@
 public class CarAccidentErrorHandling {
 
     public static void main(String[] args) {
-        try {
+        boolean majorAccident = true;
+        if(majorAccident){
             simulateCarAccident(true);
+        }
+        else{
+            simulateCarAccident(false);
+        }
+        simulateCarAccidentWithEngineFailure();
+    }
+
+    private static void simulateCarAccident(boolean majorAccident) throws AirbagDeploymentException, NoAirbagDeploymentException {
+        try {
+            if (majorAccident) {
+                throw new AirbagDeploymentException("Airbags deployed");
+            } else {
+                throw new NoAirbagDeploymentException("No airbags deployed");
+            }
         } catch (AirbagDeploymentException e) {
             System.out.println("Airbags deployed");
-        } finally {
             callAmbulance();
-        }
-
-        try {
-            simulateCarAccident(false);
         } catch (NoAirbagDeploymentException e) {
             System.out.println("No airbags deployed");
         } finally {
             cleanUpAccidentScene();
         }
+        
+    }
 
-        try {
-            simulateCarAccidentWithEngineFailure();
+    private static void simulateCarAccidentWithEngineFailure() throws EngineFailureException {
+        try{
+            throw new EngineFailureException("Car engine malfunctioned during the accident");
         } catch (EngineFailureException e) {
             System.out.println("Car engine malfunctioned during the accident");
         } finally {
             towCar();
         }
-    }
-
-    private static void simulateCarAccident(boolean majorAccident) throws AirbagDeploymentException, NoAirbagDeploymentException {
-        if (majorAccident) {
-            throw new AirbagDeploymentException("Airbags deployed");
-        } else {
-            throw new NoAirbagDeploymentException("No airbags deployed");
-        }
-    }
-
-    private static void simulateCarAccidentWithEngineFailure() throws EngineFailureException {
-        throw new EngineFailureException("Car engine malfunctioned during the accident");
     }
 
     private static void callAmbulance() {
